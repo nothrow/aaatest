c11680d607a4e3cc2c7a64269b3d9666d434b893
Vita Tauer <vita@nothrow.cz>
2017-03-26 23:01:19 +0200

    Added Check.NotNull() + R# settings

---
+++ src/Shared/Check.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+using System.Diagnostics;
+using JetBrains.Annotations;
+
+namespace aaatest
+{
+    internal static class Check
+    {
+        [ContractAnnotation("value:null => void")]
+        [ContractAnnotation("value:notnull => notnull")]
+        [DebuggerStepThrough]
+        public static T NotNull<T>([NoEnumeration] T value, [NotNull] string parameterName)
+        {
+            if (ReferenceEquals(value, null))
+                throw new ArgumentNullException(parameterName);
+
+            return value;
+        }
+    }
+}
